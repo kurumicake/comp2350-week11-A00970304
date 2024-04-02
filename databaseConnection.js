b8739ed1e1363b59bd45ee2f09d522f28b18fe05
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-04-02 14:20:55 -0700

    trying to connect to render

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,11 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
-const is_render = process.env.IS_RENDER || false;
+const is_render = process.env.IS_RENDER || true;
 const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.t1y1wqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
 if (is_render) {
-var database = new MongoClient(renderURI, {useNewUrlParser: true, useUnifiedTopology: true});
+var database = new MongoClient(renderURI, {useNewUrlParser: true, useUnifiedTopology: true, ssl: true});
 }
 else {
-var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
+var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true, ssl: true});
 }
 module.exports = database;
