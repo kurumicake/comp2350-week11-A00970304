688e9e82a88c0a210d96948dc36a6aeda88608e1
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-04-02 15:26:56 -0700

    troubleshooting

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,7 +2,8 @@ const MongoClient = require("mongodb").MongoClient;
 const is_render = process.env.IS_RENDER === true;
 const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.t1y1wqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
-
+console.log(`is_render: ${is_render}`);
+console.log(`Connecting to MongoDB with URI: ${mongoURI}`);
 const mongoURI = is_render ? renderURI : localURI;
 const database = new MongoClient(mongoURI, {
     useNewUrlParser: true, 
