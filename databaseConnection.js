aad9a8e225326ff2f234a0f3c021d834f303d756
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-04-02 15:36:27 -0700

    removing serverapiversion

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,4 +1,4 @@
-const {MongoClient, ServerApiVersion} = require("mongodb").MongoClient;
+const {MongoClient} = require("mongodb").MongoClient;
 const is_render = process.env.IS_RENDER === true;
 const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.t1y1wqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
@@ -8,7 +8,6 @@ const mongoURI = is_render ? renderURI : localURI;
 const database = new MongoClient(mongoURI, {
     // useNewUrlParser: true, 
     // useUnifiedTopology: true, 
-    serverApi: ServerApiVersion.v1,
     ssl: is_render 
 });
 console.log(`Connecting to MongoDB with URI: ${mongoURI}`);
