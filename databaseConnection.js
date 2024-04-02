1a3e08f8dd2c0e02d684637abd0b3103be5a7109
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-04-02 15:54:44 -0700

    testing connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,18 @@
 const MongoClient = require("mongodb").MongoClient;
 
+// Correctly determine if the application is running on Render
 const is_render = process.env.IS_RENDER === 'true';
-const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.t1y1wqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
-console.log(`is_render: ${is_render}`);
 
-// Choose the URI based on the value of is_render
-const mongoURI = is_render ? localURI : renderURI;
-// Use SSL if connecting to the Render URI
-const sslOption = is_render;
-const database = new MongoClient(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: sslOption
-});
+// Ensure the correct URI is selected based on the environment
+const mongoURI = is_render ? renderURI : localURI;
+
+// Logging for debugging
+console.log(`is_render: ${is_render}`);
 console.log(`Connecting to MongoDB with URI: ${mongoURI}`);
+
+// Initialize the MongoClient with the appropriate URI and SSL option
+const database = new MongoClient(mongoURI, {
+    ssl: is_render // Assuming you need SSL only when connecting to the cloud database
+});
+
 module.exports = database;
