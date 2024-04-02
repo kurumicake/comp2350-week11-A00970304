9d0ceaeff5d62867febc3a0d749a05b9ceb067c2
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-04-02 12:58:37 -0700

    third commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const is_render = process.env.IS_RENDER || false;
-const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.t1y1wqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
 if (is_render) {
 var database = new MongoClient(renderURI, {useNewUrlParser: true, useUnifiedTopology: true});
