105650fdfdc1bb9a1c6f4ef5c554b603e4c6c776
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-04-02 15:42:31 -0700

    update index

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-//Define the include function for absolute file name
+const express = require('express');
 global.base_dir = __dirname;
 global.abs_path = function(path) {
-	return base_dir + path;
+    return base_dir + path;
 }
 global.include = function(file) {
-	return require(abs_path('/' + file));
+    return require(abs_path('/' + file));
 }
 
-
-
-const express = require('express');
 const database = include('databaseConnection');
 const router = include('routes/router');
-
+const app = express();
 const port = process.env.PORT || 3000;
 
-async function connectToMongo() {
-	try {
-	  await database.connect();
-	  console.log("Successfully connected to MongoDB");
-	} catch (ex) {
-	  console.log("Error connecting to MongoDB");
-	  console.log(ex);
-	}
-  }
-  
-  connectToMongo();
-
-
-const app = express();
 app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
-app.use('/',router);
+app.use('/', router);
 
-app.listen(port, () => {
-	console.log("Node application listening on port "+port);
-}); 
+async function startServer() {
+    try {
+        await database.connect();
+        console.log("Successfully connected to MongoDB.");
 
+        app.listen(port, () => {
+            console.log(`Node application listening on port ${port}`);
+        });
+    } catch (ex) {
+        console.error("Error connecting to MongoDB", ex);
+        process.exit(1); // Stop the process if we can't connect to the database
+    }
+}
 
-
+startServer();
